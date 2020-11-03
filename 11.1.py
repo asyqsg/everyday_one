5f02abfb6a0f274851db4b2e27098aa097815825
asyqsg <43290140+asyqsg@users.noreply.github.com>
2020-11-03 11:07:20 +0800

    补卡11.1

--- 11.1.py
+++ 11.1.py
@@ -1,17 +1,21 @@
 class Solution:
     def wordBreak(self, s: str, wordDict):
         res = []
+        demo = [1] * (len(s)+1)
         wordDict = set(wordDict)
 
         def dfs(wordDict,temp,pos):
+            nums = len(res)
             if pos == len(s):
                 res.append(' '.join(temp))
                 return
             for i in range(pos,len(s)+1):
-                if s[pos:i] in wordDict:
+                if s[pos:i] in wordDict and demo[i]:
                     temp.append(s[pos:i])
                     dfs(wordDict,temp,i)
                     temp.pop()
+            demo[pos] = 1 if len(res) > nums else 0
+
 
         dfs(wordDict,[],0)
         return res
