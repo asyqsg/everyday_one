3c78c56b2d8973ec1f883e0893892d1866afab6a
asyqsg <43290140+asyqsg@users.noreply.github.com>
2020-11-06 11:11:07 +0800

    每日一题2020/11/6

---
+++ 每日一题/1356根据数字二进制下 1 的数目排序.py
@@ -0,0 +1,16 @@
+from collections import defaultdict as d
+class Solution:
+    def sortByBits(self, arr):
+        if not arr: return []
+
+        d1 = d(list)
+        for i in arr:
+            d1[bin(i).count('1')].append(i)
+        d1 = sorted(d1.items(),key=lambda x:x[0])
+        res = []
+        for key,val in d1:
+            res.extend(sorted(val))
+        return res
+if __name__ =='__main__':
+    print(Solution().sortByBits([0,1,2,3,4,5,6,7,8]))
+
