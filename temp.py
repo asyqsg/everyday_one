602969f8111771ce680522f255fe5aca1896b0ec
asyqsg <43290140+asyqsg@users.noreply.github.com>
2020-11-08 22:37:28 +0800

    updata11/8

--- temp.py
+++ temp.py
@@ -1,4 +1,26 @@
-a = 0
-b = 1000000000
-c = (1000000001*1000000000)//2
-print(c)
+n = int(input())
+nums = list(map(int, input().split()))
+
+
+def quick_sort(nums, l, r):
+    if l >= r: return
+    flag_num = nums[l]
+    i, j = l - 1, r + 1
+    while i < j:
+        while True:
+            i += 1
+            if nums[i] >= flag_num:
+                break
+        while True:
+            j -= 1
+            if nums[j] <= flag_num:
+                break
+        if i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+
+    quick_sort(nums, l, j)
+    quick_sort(nums,j + 1, r)
+    return nums
+
+
+print(quick_sort(nums, 0, len(nums)-1))
--- 每日一题/32.最长有效括号.py
+++ 每日一题/32.最长有效括号.py
@@ -1,25 +1,34 @@
+'''
+太难了！！！！
+'''
+
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        if not s: return 0
-        stack = []
         s = list(s)
-        count = 0
-        pre_count = 0
-        for i in s:
-            if i == '(':
-                if not stack:
-                    pre_count += count
-                    count = 0
-                stack.append(i)
-
-            elif i == ')':
-                if stack:
-                    stack.pop()
-                    count += 2
+        size = len(s)
+        if size < 2: return 0
+        dp = [0 for i in range(size)]
+        for i in range(size):
+            if s[i] == '(':
+                dp[i] = 0
+            else:
+                if i == 0:
+                    dp[i] = 0
                 else:
-                    pre_count += count
-                    count = 0
-        return max(pre_count,count)
-
+                    if s[i-1] == ')':
+                        index = i-dp[i-1]-1
+                        if index >= 0:
+                            if s[index] == '(':
+                                index2 = index-1
+                                if index2>=0:
+                                    dp[i] = dp[index2] + dp[i-1] + 2
+                                else:
+                                    dp[i] = dp[i-1]+2
+                        else:
+                            dp[i] = 0
+                    else:
+                        index = i-2
+                        dp[i] = dp[index] + 2
+        return max(dp)
 if __name__ == '__main__':
-    print(Solution().longestValidParentheses('()()'))
+    print(Solution().longestValidParentheses('())'))
